210af95a9e98404dfc955d263cbaae0542255027
Chaitanya Kanchan <kanchanchaitanya@gmail.com>
2018-03-25 18:58:59 +0100

    Initial Commit

---
+++ Tupshar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tupshar
+//
+//  Created by Chaitanya Kanchan on 25/03/2018.
+//  Copyright © 2018 Chaitanya Kanchan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tupshar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tupshar
+//
+//  Created by Chaitanya Kanchan on 25/03/2018.
+//  Copyright © 2018 Chaitanya Kanchan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
